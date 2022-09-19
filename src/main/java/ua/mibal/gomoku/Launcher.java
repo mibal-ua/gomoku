fcb657e65f30d19cde799209d7396f3247043c75
mibal_ua <9mohapx9@gmail.com>
2022-09-19 12:13:55 +0200

    Add basic logic of app

--- src/main/java/ua/mibal/gomoku/Launcher.java
+++ src/main/java/ua/mibal/gomoku/Launcher.java
@@ -24,6 +24,8 @@ package ua.mibal.gomoku;
 public class Launcher {
 
     public static void main(String[] args) {
-        // Add code here
+        final GameFactory gameFactory = new GameFactory(args);
+        final Game game = gameFactory.create();
+        game.start();
     }
 }
