bdb24969821657744e74e4c84d45fc043a032249
mibal_ua <9mohapx9@gmail.com>
2022-09-19 15:06:32 +0200

    Add oppositeSign method to the Sign class

--- src/main/java/ua/mibal/gomoku/model/game/Sign.java
+++ src/main/java/ua/mibal/gomoku/model/game/Sign.java
@@ -10,5 +10,24 @@ public enum Sign {
 
     X,
 
-    O
+    O;
+
+    public Sign oppositeSign() {
+        if (this == X) {
+            return O;
+        } else if (this == O) {
+            return X;
+        } else {
+            throw new IllegalStateException("Empty value does not have an opposite one.");
+        }
+    }
+
+    @Override
+    public String toString() {
+        if (this == EMPTY) {
+            return " ";
+        } else {
+            return name();
+        }
+    }
 }
