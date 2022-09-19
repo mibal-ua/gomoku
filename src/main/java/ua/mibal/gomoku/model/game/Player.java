9030fa5e476bcee7fb9171410ce9f504838dd8ad
mibal_ua <9mohapx9@gmail.com>
2022-09-19 14:47:37 +0200

    Create Player class

---
+++ src/main/java/ua/mibal/gomoku/model/game/Player.java
@@ -0,0 +1,23 @@
+package ua.mibal.gomoku.model.game;
+
+import ua.mibal.gomoku.component.Move;
+
+/**
+ * @author Michael Balakhon
+ * @link t.me/mibal_ua.
+ */
+public class Player {
+
+    private final Sign sign;
+
+    private final Move move;
+
+    public Player(final Sign sign, final Move move) {
+        this.sign = sign;
+        this.move = move;
+    }
+
+    public void makeMove(final GameTable gameTable){
+        move.make(gameTable, sign);
+    }
+}
