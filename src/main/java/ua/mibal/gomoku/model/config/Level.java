c8f228075599a680e9973ba5f259d82e2614f8d7
mibal_ua <9mohapx9@gmail.com>
2022-09-21 10:57:18 +0200

    Add strategies to Level class

--- src/main/java/ua/mibal/gomoku/model/config/Level.java
+++ src/main/java/ua/mibal/gomoku/model/config/Level.java
@@ -1,12 +1,33 @@
 package ua.mibal.gomoku.model.config;
 
+import ua.mibal.gomoku.component.ComputerMoveStrategy;
+import ua.mibal.gomoku.component.strategy.PreventUserWinComputerMoveStrategy;
+import ua.mibal.gomoku.component.strategy.RandomComputerMoveStrategy;
+import ua.mibal.gomoku.component.strategy.WinNowComputerMoveStrategy;
+import ua.mibal.gomoku.component.strategy.WinOnTheNextStepComputerMoveStrategy;
+
 /**
  * @author Michael Balakhon
  * @link t.me/mibal_ua.
  */
 public enum Level {
+    
+    LEVEL1(new ComputerMoveStrategy[]{
+            new RandomComputerMoveStrategy()}),
 
-    LEVEL1,
+    LEVEL2(new ComputerMoveStrategy[]{
+            new WinNowComputerMoveStrategy(),
+            new PreventUserWinComputerMoveStrategy(),
+            new WinOnTheNextStepComputerMoveStrategy(),
+            new RandomComputerMoveStrategy()});
 
-    LEVEL2
+    private final ComputerMoveStrategy[] strategies;
+
+    Level(final ComputerMoveStrategy[] strategies) {
+        this.strategies = strategies;
+    }
+
+    public ComputerMoveStrategy[] getStrategies() {
+        return strategies.clone();
+    }
 }
